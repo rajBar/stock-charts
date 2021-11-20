9fd3e9ef54169684652bba16d3d9c08cadaf7503
Gulraj Bariah <raj.uni@outlook.com>
2021-11-20 22:06:46 +0000

    first commit - issues around selectors in saga not picking up data in the correct way

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { configureStore } from './store/configureStore';
+import { Provider } from 'react-redux';
+import { initialState } from './store/initialState';
 import reportWebVitals from './reportWebVitals';
 
+const store = configureStore(initialState);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
